59be3fbef8eeec7728784be49ec26bdc9b0b4823
sara-alsayed-632 <444052896@std.psau.edu.sa>
2026-05-06 21:13:34 +0300

    import

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
+
 
 // ANSI Color Codes for enhanced terminal output
 class Colors {
